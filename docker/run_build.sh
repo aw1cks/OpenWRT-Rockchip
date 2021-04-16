4f0e5eea10a25ba32fc0a8efdad7cd9d68317ff2
Alex Wicks <alex@awicks.io>
2021-04-16 09:20:23 +0100

    Only output R4S SquashFS; adding compose file

--- docker/run_build.sh
+++ docker/run_build.sh
@@ -8,7 +8,8 @@ let make_process=$(nproc)+1
 
 
 # Presemuption: git repo present at /build/tmp
-rm -rfv /build/tmp/openwrt
+# Clean current working dir and check out fresh 21.02 src
+rm -rf /build/tmp/openwrt
 git clone \
   -b openwrt-21.02 \
   --single-branch \
@@ -16,37 +17,38 @@ git clone \
   /build/tmp/openwrt
 
 
+# Copy build scripts
 cd /build/tmp/openwrt
 cp -r ../step/* ./
 
 
+# Run build scripts
 bash 00-prepare_openwrt.sh
 bash 01-prepare_package.sh
 bash 03-create_acl_for_luci.sh -a
 
 
-cp ../seed/rockchip.seed .config
+# Copy configuration
+cp ../seed/r4s.seed .config
 make defconfig
-cp .config rockchip_multi.config
 
 
+# Build cross-compile toolchain
 make toolchain/install -j${make_process} V=s || make toolchain/install -j1 V=s
 
 
+# Build release
 make -j${make_process} V=s || make -j${make_process} V=s || make -j1 V=s
 
 
+# Package release
 cd ..
-rm -rfv ./artifact
+rm -rf ./artifact
 mkdir -p ./artifact
 mv openwrt/bin/targets/rockchip/armv8/*sysupgrade.img* ./artifact/
 cd ./artifact/
-ls -Ahl
 pigz -d *.gz && exit 0
 pigz --best *.img
-ls -Ahl
 sha256sum openwrt*r4s* | tee R4S-QC-$(date +%Y-%m-%d)-21.02.sha256sum
-zip R4S-QC-$(date +%Y-%m-%d)-21.02-ext4.zip *r4s*ext4*
-zip R4S-QC-$(date +%Y-%m-%d)-21.02-sfs.zip *r4s*squashfs*
-cp ../openwrt/*.config ./
+cp ../openwrt/.config r4s.config
 ls -Ahl
